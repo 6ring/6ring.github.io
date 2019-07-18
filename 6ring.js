f28f3db257ef24bf504aae9f421770c9151efa03
cygx <cygx@cpan.org>
2019-07-18 20:34:04 +0200

    try to deal gracefully with external links

--- 6ring.js
+++ 6ring.js
@@ -1,7 +1,18 @@
-addEventListener("message", function(event) {
+window.addEventListener('message', function(event) {
    if(event.origin === 'https://6ring.github.io') switch(event.data) {
         case 'exit':
-        parent.location = self.location;
+        top.location = self.location;
         break;
+    }
+}, false);
+
+document.addEventListener('click', function(event) {
+    if(event.target.nodeName.toUpperCase() !== 'A'
+        || event.ctrlKey || event.shiftKey) return;
+
+    let url = event.target.href;
+    if(new URL(url, location.href).origin !== location.origin) {
+        event.preventDefault();
+        top.location.href = url;
     }
 }, false);
