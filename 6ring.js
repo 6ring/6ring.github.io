13df2d4dad5cd4382859b592d0023ad9fae252e2
cygx <cygx@cpan.org>
2019-07-20 11:52:21 +0200

    embed widget egneration onto 6ring.js

--- 6ring.js
+++ 6ring.js
@@ -1,4 +1,26 @@
 const SIXRING_ID = document.currentScript.dataset.sixringId;
+const SIXRING_WIDGET = `<span style="display: inline-block" id="sixring-widget"
+><button
+  data-href="https://6ring.github.io/"
+  onclick="location.href = this.dataset.href"
+  style="cursor: pointer; border: 0 none; background: transparent; display: block"
+  title="visit ring homepage">🦋 <b>Perl 6</b> webring</button
+><button
+  data-href="https://6ring.github.io/prev?${SIXRING_ID}"
+  onclick="location.href = this.dataset.href"
+  style="cursor: pointer; width: 6em"
+  title="visit previous site in ring">⮜⮜ prev</button
+><button
+  data-href="https://6ring.github.io/?${SIXRING_ID}"
+  onclick="location.href = this.dataset.href"
+  style="cursor: pointer; width: 6em"
+  title="load ring toolbar">🔗 load</button
+><button
+  data-href="https://6ring.github.io/next?${SIXRING_ID}"
+  onclick="location.href = this.dataset.href"
+  style="cursor: pointer; width: 6em"
+  title="visit next site in ring">next ⮞⮞</button
+></span>`;
 
 addEventListener('DOMContentLoaded', function(event) {
     if(top.name === 'sixring') {
